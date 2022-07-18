1bcb14abdb11b9fc4b7cb3a3a7f25b1d0dbbf8da
Wagner <wagnerdmll1994@gmail.com>
2022-07-18 13:35:40 -0300

    Node CORS setup - Security layer added

--- petbook/backend/server.js
+++ petbook/backend/server.js
@@ -1,15 +1,35 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const app = express()
+let allowed = ["http://localhost:3000", "another link"];
 
-app.get('/', (req, res) => {
-	res.send('Welcome to the server...')
-})
+function options(req, res) {
+	let tmp;
+	let origin = req.header("Origin");
 
-app.get('/example', (req, res) => {
-	res.send('Example...')
-})
+	if (allowed.indexOf(origin) > -1) {
+		tmp = {
+			origin: true,
+			optionSuccessStatus: 200,
+		};
+	} else {
+		tmp = {
+			origin: "don't exist",
+		};
+	}
+	res(null, tmp);
+}
+app.use(cors(options));
+
+app.get("/", (req, res) => {
+	res.send("Welcome to the server...");
+});
+
+app.get("/example", (req, res) => {
+	res.send("Example...");
+});
 
 app.listen(8000, () => {
-	console.log('Server is listening')
-})
+	console.log("Server is listening");
+});
