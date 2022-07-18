a231a599bf79a386849aef283450e8eb93fc7310
Wagner <wagnerdmll1994@gmail.com>
2022-07-18 14:15:52 -0300

    Facebook - Routes setup

--- facebook/backend/server.js
+++ facebook/backend/server.js
@@ -1,33 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+
+const { readdirSync } = require("fs");
 const app = express();
-let allowed = ["http://localhost:3000", "some other link"];
+app.use(cors());
 
-function options(req, res) {
-	let tmp;
-	let origin = req.header("Origin");
-	if (allowed.indexOf(origin) > -1) {
-		tmp = {
-			origin: true,
-			optionSuccessStatus: 200,
-		};
-	} else {
-		tmp = {
-			origin: "do not exist",
-		};
-	}
-	res(null, tmp);
-}
-
-app.use(cors(options));
-
-app.get("/", (req, res) => {
-	res.send("welcome from home");
-});
-
-app.get("/books", (req, res) => {
-	res.send("welcome from books");
-});
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
 app.listen(8000, () => {
 	console.log("Server is listening...");
