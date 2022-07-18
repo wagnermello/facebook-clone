8a3ba50096d15d04314bd1ec547dcff0e935a1e2
Wagner <wagnerdmll1994@gmail.com>
2022-07-18 16:44:16 -0300

    Connecting to MongoDB

--- facebook/backend/server.js
+++ facebook/backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -7,9 +8,18 @@ const { readdirSync } = require("fs");
 const app = express();
 app.use(cors());
 
+//routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//database
+mongoose
+	.connect(process.env.DATABASE_URL, {
+		useNewUrlParser: true,
+	})
+	.then(() => console.log("Database connected successfuly"))
+	.catch((err) => console.log("Error connecting to MongoDB", err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-	console.log(`server is running on port ${PORT}...`);
+	console.log(`Server is running on port ${PORT}...`);
 });
