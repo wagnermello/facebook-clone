49e8c8db4a51e9b7c7ad39d7cbc2da8c51687644
Wagner <wagnerdmll1994@gmail.com>
2022-07-19 09:37:48 -0300

    Devbook - connecting to MongoDB

--- devbook/backend/server.js
+++ devbook/backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
 
 const { readdirSync } = require("fs");
@@ -8,7 +9,16 @@ const app = express();
 
 app.use(cors());
 
+//routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//database
+mongoose
+	.connect(process.env.DATABASE_URL, {
+		useNewUrlParser: true,
+	})
+	.then(() => console.log("Database connected successfuly"))
+	.catch((err) => console.log("Error to conect to MongoDB.\n", err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
