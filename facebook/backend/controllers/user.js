4d897b30e184a1b71ec3ebf449e9bc574faaefc3
Wagner <wagnerdmll1994@gmail.com>
2022-07-19 15:39:27 -0300

    Facebook - email regex validation | encrypt passwords with bcrypt | validate length

--- facebook/backend/controllers/user.js
+++ facebook/backend/controllers/user.js
@@ -1,6 +1,68 @@
-exports.home = (req, res) => {
-	res.json({
-		message: "message test",
-		error: "error test",
-	});
+const { validateEmail, validateLength } = require("../helpers/validation");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+exports.register = async (req, res) => {
+	try {
+		const {
+			first_name,
+			last_name,
+			email,
+			password,
+			username,
+			bYear,
+			bMonth,
+			bDay,
+			gender,
+		} = req.body;
+
+		if (!validateEmail(email)) {
+			return res.status(400).json({
+				message: "Invalid email address",
+			});
+		}
+		const check = await User.findOne({ email });
+		if (check) {
+			return res.status(400).json({
+				message:
+					"This email address already exists, try with a different email address",
+			});
+		}
+
+		if (!validateLength(first_name, 3, 30)) {
+			return res.status(400).json({
+				message: "First name must contain between 3 and 30 characters.",
+			});
+		}
+
+		if (!validateLength(last_name, 3, 30)) {
+			return res.status(400).json({
+				message: "Last name must contain between 3 and 30 characters.",
+			});
+		}
+
+		if (!validateLength(password, 6, 40)) {
+			return res.status(400).json({
+				message: "Password must be atleast 6 characters.",
+			});
+		}
+		const cryptedPassword = await bcrypt.hash(password, 12);
+		console.log(cryptedPassword);
+		return;
+
+		const user = await new User({
+			first_name,
+			last_name,
+			email,
+			password,
+			username,
+			bYear,
+			bMonth,
+			bDay,
+			gender,
+		}).save();
+		res.json(user);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
