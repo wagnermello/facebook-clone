064b68a9e8d7b4478ec06b771790092379fdb052
Wagner <wagnerdmll1994@gmail.com>
2022-07-20 13:58:31 -0300

    Facebook - setup react router dom

--- facebook/frontend/src/index.js
+++ facebook/frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById("root")
 );
