cc42d66479f6670d161471f4e4ecca8586814d73
Wagner <wagnerdmll1994@gmail.com>
2022-07-20 10:46:20 -0300

    Facebook - Send email verifications and finish registration of users

--- facebook/backend/controllers/user.js
+++ facebook/backend/controllers/user.js
@@ -6,6 +6,7 @@ const {
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/tokens");
+const { sendVerificationEmail } = require("../helpers/mailer");
 
 exports.register = async (req, res) => {
 	try {
@@ -70,9 +71,21 @@ exports.register = async (req, res) => {
 			{ id: user._id.toString() },
 			"30m"
 		);
-		console.log(emailVerificationToken);
 
-		res.json(user);
+		const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+		sendVerificationEmail(user.email, user.first_name, url);
+
+		const token = generateToken({ id: user._id.toString() }, "7d");
+		res.send({
+			id: user._id,
+			username: user.username,
+			picture: user.picture,
+			first_name: user.first_name,
+			last_name: user.last_name,
+			token: token,
+			verified: user.verified,
+			message: "Register succsess! Please active your email.",
+		});
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
